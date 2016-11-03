12889a890e028cff5dfc4b15147882d1b4386a22
rodu <greenheron35@gmail.com>
2016-11-03 14:28:03 +0000

    (WIP) Implementing data refresh

--- quakemap.js
+++ quakemap.js
@@ -2,36 +2,62 @@
 (function(Rx){
   'use strict';
 
+  const FETCH_INTERVAL = 5000;
+
   const QUAKE_URL = (
     //'//earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojsonp'
     'http://localhost:8080/all_day.geojsonp'
   );
   const map = L.map('map');
 
+  //map.setView([0, 0], 7);
+
   L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
-  const quakes = Rx.DOM.jsonpRequest({
-    url: QUAKE_URL,
-    jsonpCallback: 'eqfeed_callback'
-  })
-  .flatMap((result) => {
-    return Rx.Observable.from(result.response.features);
-  })
-  .map((quake) => {
-    const properties = quake.properties;
+  const jsonStream = () => {
+    return Rx.DOM.jsonpRequest({
+      url: QUAKE_URL,
+      jsonpCallback: 'eqfeed_callback'
+    });
+  };
 
-    return {
-      lat: quake.geometry.coordinates[1],
-      lng: quake.geometry.coordinates[0],
-      mag: properties.mag,
-      place: properties.place,
-      time: properties.time
-    };
-  });
+  const quakeStream = Rx.Observable
+    .interval(FETCH_INTERVAL)
+    .startWith(1)
+    .flatMap(() => jsonStream());
 
-  quakes.subscribe((quake) => {
+  const quakeMap = (Observable) => {
+    return Observable
+      .flatMap((result) => {
+        return Rx.Observable.from(result.response.features);
+      })
+      .distinct((feature) => {
+        return feature.properties.code;
+      })
+      .map((feature) => {
+        return {
+          lat: feature.geometry.coordinates[1],
+          lng: feature.geometry.coordinates[0],
+          mag: feature.properties.mag,
+          place: feature.properties.place,
+          time: feature.properties.time
+        };
+      });
+  };
+
+  function findMaxByProp(prop){
+    return (x, y) => (x[prop] > y[prop]) ? 1 : (x[prop] < y[prop]) ? -1 : 0;
+  }
+
+  quakeMap(jsonStream())
+    .max(findMaxByProp('mag'))
+    .subscribe((quake) => {
+      map.setView([quake.lat, quake.lng], 7);
+    });
+
+  quakeMap(quakeStream).subscribe((quake) => {
     const popupData = '' +
       '<h3>' + quake.place + '</h3>' +
       '<ul>' +
@@ -48,10 +74,6 @@
       .bindPopup(popupData);
   });
 
-  function findMaxByProp(prop){
-    return (x, y) => (x[prop] > y[prop]) ? 1 : (x[prop] < y[prop]) ? -1 : 0;
-  }
-
   function centerMapByProp(prop){
     quakes
       .max(findMaxByProp(prop))
@@ -66,8 +88,8 @@
     .subscribe((event) => centerMapByProp(event.target.value));
 
   // Let's centre on the currently selected filter
-  Rx.Observable.from(getFilterRadios())
+  /*Rx.Observable.from(getFilterRadios())
     .filter((radio) => radio.checked)
     .subscribe((radio) => centerMapByProp(radio.value));
-
+*/
 })(Rx);
