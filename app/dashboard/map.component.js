f6aede877fe5707500c4b162952a6da5521989a7
rodu <greenheron35@gmail.com>
2016-11-04 10:41:15 +0000

    Streams quakes data using RxJS
    Draws the quakes on the map and only updates for new quakes.
    
    Needs to remove previous quakes that are not in the data anymore.

--- app/dashboard/map.component.js
+++ app/dashboard/map.component.js
@@ -10,6 +10,39 @@ function MapController(L, quakesService){
   let map;
   let markers;
   let circles;
+  let strongestQuake;
+
+  const drawQuake = (quake) => {
+    const popupData = '' +
+      '<h3>' + quake.place + '</h3>' +
+      '<ul>' +
+        '<li><strong>Time:</strong> ' +
+          new Date(quake.time).toUTCString() +
+        '</li>' +
+        '<li><strong>Magnitude:</strong> ' + quake.mag + '</li>' +
+      '</ul>';
+
+    circles[quake.code] = L.circle(
+      [quake.lat, quake.lng],
+      quake.mag * 1000
+    ).addTo(map);
+
+    markers[quake.code] = L.marker([quake.lat, quake.lng])
+      .addTo(map)
+      .bindPopup(popupData);
+  };
+
+  const strongestSoFar = (quake) => {
+    if (!strongestQuake){
+      strongestQuake = quake;
+      return;
+    }
+
+    if (quake.mag > strongestQuake.mag){
+      map.setView([quake.lat, quake.lng], 7);
+      strongestQuake = quake;
+    }
+  };
 
   this.$onInit = () => {
     map = L.map('map');
@@ -23,10 +56,10 @@ function MapController(L, quakesService){
       `
     }).addTo(map);
 
-    quakesService.loadQuakes()
-      .subscribe((value) => {
-        console.log(value);
-      });
+    map.setView([0, 0], 7);
+
+    quakesService.getQuakesStream().subscribe(strongestSoFar);
+    quakesService.getQuakesStream().subscribe(drawQuake);
   };
 }
 
--- app/dashboard/quakes.service.js
+++ app/dashboard/quakes.service.js
@@ -3,12 +3,46 @@ import angular from 'angular';
 quakesService.$inject = ['Rx'];
 function quakesService(Rx){
 
-  function loadQuakes() {
-    return Rx.Observable.just('one');
+  const FETCH_INTERVAL = 5000; //1800e+3; // every half hour
+
+  const QUAKE_URL = (
+    //'//earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson'
+    'http://localhost:8080/all_day.geojsonp'
+  );
+
+  function getQuakesStream() {
+    const jsonStream = () => {
+      return Rx.DOM.jsonpRequest({
+        url: QUAKE_URL,
+        jsonpCallback: 'eqfeed_callback'
+      });
+    };
+
+    const quakeStream = Rx.Observable
+      .interval(FETCH_INTERVAL)
+      .startWith(1)
+      .flatMap(() => jsonStream())
+      .flatMap((result) => {
+        return Rx.Observable.from(result.response.features);
+      })
+      .distinct((feature) => {
+        return feature.properties.code;
+      })
+      .map((feature) => {
+        return {
+          lat: feature.geometry.coordinates[1],
+          lng: feature.geometry.coordinates[0],
+          mag: feature.properties.mag,
+          place: feature.properties.place,
+          time: feature.properties.time
+        };
+      });
+
+    return quakeStream;
   };
 
   return {
-    loadQuakes
+    getQuakesStream
   }
 }
 
