fd8f9a304e8514d0d3d5b476b2fed4a7ef5f6c7c
rodu <greenheron35@gmail.com>
2016-11-06 16:35:59 +0000

    Adds gulp task to concatenate assets

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,31 @@ var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var connect = require('gulp-connect');
 var rollup = require('rollup-stream');
+var concat = require('gulp-concat');
 
 var HTML_FILES = ['index.html'];
 
 gulp.task('html', function () {
   gulp.src(HTML_FILES).pipe(connect.reload());
+});
+
+gulp.task('vendors', function () {
+  return gulp.src([
+      './lib/jquery.min.js',
+      './lib/jquery.dataTables.min.js',
+      './lib/angular.min.js',
+      './lib/angular-datatables.min.js',
+      './lib/angular-datatables.bootstrap.min.js',
+      'rc="./lib/bootstrap.min.js"></sc',
+      './lib/lodash.min.js',
+      './lib/rx.all.min.js',
+      './lib/rx.dom.min.js',
+      './lib/ie10-viewport-bug-workaround.js',
+      './lib/leaflet.js'
+    ])
+    .pipe(concat('vendors.bundle.js'))
+    .pipe(gulp.dest('.'));
+
 });
 
 gulp.task('serve', function() {
@@ -21,13 +41,14 @@ gulp.task('bundle', function () {
     .pipe(source('quakemap.bundle.js'))
     .pipe(gulp.dest('.'))
     .pipe(connect.reload());
-})
+});
 
 gulp.task('watch', function () {
   gulp.watch('./app/**/*.js', ['bundle']);
   gulp.watch(HTML_FILES, ['html']);
 });
 
-gulp.task('development', ['bundle', 'serve', 'watch']);
+gulp.task('build', ['vendors', 'bundle']);
+gulp.task('development', ['build', 'serve', 'watch']);
 
-gulp.task('default', ['bundle'])
+gulp.task('default', ['build']);
